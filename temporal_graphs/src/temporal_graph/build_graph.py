fc879ab8ca962b9c7e3a079bd1c7d96b0720bf5a
Marat Shikhragimov <m.shikhragimov@gmail.com>
2022-12-16 15:09:54 +0200

    fix in graph building

--- temporal_graphs/src/temporal_graph/build_graph.py
+++ temporal_graphs/src/temporal_graph/build_graph.py
@@ -3,23 +3,25 @@ import torch
 from torch_geometric.data import HeteroData
 
 
-def create_torch_temporal_graph_from_df(graph, save=True, path_prefix=""):
-    nodes = set(graph["node_1"])
-    nodes.update(set(graph["node_2"]))
+def create_torch_temporal_graph_from_df(graph, save=True, path_prefix="", feature_size=4):
+    nodes = set(graph["node_1"].unique())
+    nodes.update(set(graph["node_2"].unique()))
     nodes = list(nodes)
-    features = np.random.rand(len(nodes), 4)
+    print(f"number of nodes: {len(nodes)}")
+    features = np.random.rand(len(nodes), feature_size)
 
     data = HeteroData()
     data["node"].x = torch.tensor(features, dtype=torch.float)
     for i, date_group in enumerate(np.sort(graph["date_group"].unique())):
         data["node", f"in_date_group_{date_group}", "node"].edge_index = torch.tensor(
-            np.array([graph[graph["date_group"] == date_group]["node_1"].values,
-                      graph[graph["date_group"] == date_group]["node_2"].values]),
-            dtype=torch.long,
-        )
-        data["node", f"in_date_group_{date_group}", "node"].edge_index = torch.tensor(
-            np.array([graph[graph["date_group"] == date_group]["node_2"].values,
-                      graph[graph["date_group"] == date_group]["node_1"].values]),
+            np.array([
+                np.append(
+                    graph[graph["date_group"] == date_group]["node_1"].values,
+                    graph[graph["date_group"] == date_group]["node_2"].values),
+                np.append(
+                    graph[graph["date_group"] == date_group]["node_2"].values,
+                    graph[graph["date_group"] == date_group]["node_1"].values)
+            ]),
             dtype=torch.long,
         )
     if save:
