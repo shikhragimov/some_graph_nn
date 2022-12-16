eab2e2efa8336c1b44321b8825bd9dd8c9bf5ad4
Marat Shikhragimov <m.shikhragimov@gmail.com>
2022-12-16 15:10:25 +0200

    better mock data

--- temporal_graphs/src/data/mock_data.py
+++ temporal_graphs/src/data/mock_data.py
@@ -10,6 +10,12 @@ def get_temporal_mock_graph():
         pd.DataFrame
     """
     return pd.DataFrame({
-        "node_1": np.random.randint(6, size=100),
-        "node_2": np.random.randint(6, size=100),
-        "date_group": np.random.randint(5, size=100)})
+        "node_1": np.concatenate([
+            np.random.randint(20, size=100),
+            np.random.randint(20, 40, size=100),
+            np.random.randint(40, 60, size=100)]),
+        "node_2": np.concatenate([
+            np.random.randint(20, size=100),
+            np.random.randint(20, 40, size=100),
+            np.random.randint(40, 60, size=100)]),
+        "date_group": np.random.randint(1, size=300)})
